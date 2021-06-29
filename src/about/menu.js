402f753e144795fd13fc876c67ae5000e1e773a8
arthurGC <aguzmanc@unsa.edu.pe>
2021-06-29 16:20:18 -0500

    Fix indentation JS

--- src/about/menu.js
+++ src/about/menu.js
@@ -3,7 +3,7 @@ export const menu = document.querySelector('.menu');
 export const menuLinks = [...document.querySelectorAll('header nav ul li a')];
 
 export const openCloseMenu = () => {
-    menu.classList.toggle('active');
-    hambIcon.classList.toggle('fa-bars');
-    hambIcon.classList.toggle('fa-times');
-}
+  menu.classList.toggle('active');
+  hambIcon.classList.toggle('fa-bars');
+  hambIcon.classList.toggle('fa-times');
+};
